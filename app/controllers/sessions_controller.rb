8b752cb5dce6dfb9daa329a650dca56e788be1a4
Swami Atma <swami@ohlalaweb.com>
2013-01-27 14:04:32 +0100

    The user logging in from any page is being redirected to that page upon logging in.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,12 @@
 class SessionsController < ApplicationController
 
   def new
+    redirect_to root_path if current_user
   end
 
   def create
     user = User.find_by_email(params[:email])
+    set_target_page
     if user && user.authenticate(params[:password])
       if params[:remember_me]
         cookies.permanent[:auth_token] = user.auth_token
@@ -22,6 +24,12 @@ class SessionsController < ApplicationController
   def destroy
     cookies.delete(:auth_token)
     redirect_to safe_root_url, notice: t('authentication.logout_confirmation')
+  end
+  
+  private
+  
+  def set_target_page
+    session[:target_page] = request.referer unless session[:target_page] # && !request.referer.nil?
   end
 
 end
