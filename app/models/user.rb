cc7a4485bff161dcf2bea94fb1b9f635a0f4e033
Swami Atma <swami@ohlalaweb.com>
2013-08-03 12:44:20 +0200

    Improved the click-flow for new user signups and to work in coordination with existing mailing list records.

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,9 +11,11 @@ class User < ActiveRecord::Base
 
   validates_presence_of :email
   validates_uniqueness_of :email, :case_sensitive => false
-  validates_presence_of :password, on: :create
-  validates_presence_of :first_name
-  validates_presence_of :last_name
+  # validates_presence_of :password, on: :create
+
+  # TODO - this needs to be somehow integrated in the click flow
+  # validates_presence_of :first_name
+  # validates_presence_of :last_name
 
   scope :alphabetically, order('last_name, first_name')
   scope :administrators, where('admin = ?', true)
