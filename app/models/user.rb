81999a37924b25b6e313c12ae2e7fe6c0c63cb16
Swami Atma <swami@ohlalaweb.com>
2013-10-27 19:51:24 +0100

    Fixed up scopes for Rails 4.

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,8 +17,8 @@ class User < ActiveRecord::Base
   # validates_presence_of :first_name
   # validates_presence_of :last_name
 
-  scope :alphabetically, order('last_name, first_name')
-  scope :administrators, where('admin = ?', true)
+  scope :alphabetically, -> { order('last_name, first_name') }
+  scope :administrators, -> { where('admin = ?', true) }
 
   before_create { generate_token(:auth_token) }
 
