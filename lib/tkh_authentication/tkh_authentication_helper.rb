c93ca9f7bfd6f2bb0729d8de44facc947b66645d
Swami Atma <swami@ohlalaweb.com>
2014-03-08 17:53:33 +0100

    Debugged authentication helper. find_by_attribute methods are not compatible with rails 4.

--- lib/tkh_authentication/tkh_authentication_helper.rb
+++ lib/tkh_authentication/tkh_authentication_helper.rb
@@ -1,6 +1,6 @@
 module TkhAuthenticationHelper
   def self.included(base)
-    base.send(:include, InstanceMethods) 
+    base.send(:include, InstanceMethods)
     # base.before_filter :current_user
     # base.after_filter :my_method_2
   end
@@ -8,9 +8,9 @@ module TkhAuthenticationHelper
   module InstanceMethods
     # duplicated from action controller extension. there must be a better way
     def current_user
-      @current_user ||= User.find_by_auth_token!(cookies[:auth_token]) if cookies[:auth_token]
+      @current_user ||= User.find_by!(auth_token: cookies[:auth_token]) if cookies[:auth_token]
     end
-        
+
     # duplicated from action controller extension. there must be a better way
     def administrator?
       @administrator ||= current_user && current_user.admin?
