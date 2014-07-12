c37b69797629ccd498f16219de2a7b867d343e13
Swami Atma <swami@ohlalaweb.com>
2014-07-12 11:16:02 +0200

    Moved new pathway routes inside the translation block

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
+
     get 'signup', to: 'users#new', as: 'signup'
     get 'login', to: 'sessions#new', as: 'login'
     get 'logout', to: 'sessions#destroy', as: 'logout'
@@ -13,13 +14,13 @@ Rails.application.routes.draw do
     end
     resources :sessions
     resources :password_resets
+
+    # New access control pathway
+    get '/reception', to: 'reception#email_input', as: 'email_input'
+    post '/parse_email', to: 'reception#parse_email'
+    get '/email_validation', to: 'reception#email_validation'
+    get '/create_your_password', to: 'reception#create_your_password'
+    post '/password_creation/:id', to: 'reception#password_creation', as: 'password_creation'
+
   end
-
-  # New access control pathway
-  get '/reception', to: 'reception#email_input', as: 'email_input'
-  post '/parse_email', to: 'reception#parse_email'
-  get '/email_validation', to: 'reception#email_validation'
-  get '/create_your_password', to: 'reception#create_your_password'
-  post '/password_creation/:id', to: 'reception#password_creation', as: 'password_creation'
-
 end
