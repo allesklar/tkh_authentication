732a6a11f1b7e5d21925a3b3ea0deec2f504ef8f
Swami Atma <swami@TenThousandHours.eu>
2014-09-22 19:09:46 +0200

    Built navigation around login form.

--- lib/tkh_authentication/tkh_authentication_action_controller_extension.rb
+++ lib/tkh_authentication/tkh_authentication_action_controller_extension.rb
@@ -1,11 +1,11 @@
 module TkhAuthenticationActionControllerExtension
   def self.included(base)
-    base.send(:include, InstanceMethods) 
+    base.send(:include, InstanceMethods)
   end
 
   module InstanceMethods
     def current_user
-      @current_user ||= User.find_by_auth_token!(cookies[:auth_token]) if cookies[:auth_token]
+      @current_user ||= User.find_by!(auth_token: cookies[:auth_token]) if cookies[:auth_token]
     end
 
     def authenticate
@@ -14,20 +14,20 @@ module TkhAuthenticationActionControllerExtension
         redirect_to login_url, alert: t('authentication.warning.login_needed')
       end
     end
-    
+
     def authenticate_with_admin
       unless administrator?
         session[:target_page] = request.url if session[:target_page].nil?
         redirect_to safe_root_url, alert: t('authentication.warning.restricted_access')
       end
     end
-    
+
     def administrator?
       current_user && current_user.admin?
     end
-    
+
     private
-    
+
     def safe_root_url
       defined?(root_url) ? root_url : '/'
     end
